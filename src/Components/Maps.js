d2969f0ecd0278bf67a684ac92bbe456625f292c
Juan Diego Alejandro Valencia Pena <juadiegoalejandro@hotmail.com>
2019-10-28 18:09:59 -0500

    [Fix] Rendering current position

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 let currentpos = [];
 export default class GoogleMap extends Component {
-	googleMapRef = React.createRef()
+	
+	state = {
+		'currentpos': {'lat': 0, 'lng': 0}
+	}
 
+	googleMapRef = React.createRef()
+	
 	
 	componentDidMount () {
 		const url = "https://maps.googleapis.com/maps/api/js?key=".concat(process.env.REACT_APP_GOOGLEKEY, "&libraries=places");
@@ -20,19 +25,25 @@ export default class GoogleMap extends Component {
 	createGoogleMap() {
 		console.log("about to getting position")
 		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-				currentpos.push({key: "lat", value: position.coords.latitude});
-				currentpos.push({key: "lng", value: position.coords.longitude});
+			navigator.geolocation.getCurrentPosition((position) => {
+				let currentpos = {'lat': position.coords.latitude, 'lng':position.coords.longitude}
+				currentpos = {'lat': 4.6097100, 'lng':-74.0817500}				
+				new window.google.maps.Map(this.googleMapRef.current, {
+					zoom: 16,
+					center: currentpos,
+					disableDefaultUI: false, 
+				})
+				this.setState({'currentpos': currentpos})
 				console.log("Getting position from browser")
 			});
 		} else {
 			console.log("it doesn't work");
-			currentpos.push({key: "lat", value: 4.6097100 });
-			currentpos.push({key: "lng", value: -74.0817500});
+			let currentpos = {'lat': 4.6097100, 'lng':-74.0817500}
+			this.setState({'currentpos': currentpos})
 		}
 		new window.google.maps.Map(this.googleMapRef.current, {
 			zoom: 16,
-			center: currentpos,
+			center: this.state.currentpos,
 			disableDefaultUI: false, 
 		})
 	    }
