72f352b33037d472df854d2a2268eadb718c4143
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-10-28 16:48:10 +0000

    [Fix] Changes on data

--- src/Components/2Maps.js
+++ src/Components/2Maps.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-let currentpos = [];
+const currentpos = [];
+let initialmap = { zoom: 16, center: currentpos, disableDefaultUI: False};
 export default class GoogleMap extends Component {
 	googleMapRef = React.createRef()
-
-	
+    
 	componentDidMount () {
 		const url = "https://maps.googleapis.com/maps/api/js?key=".concat(process.env.REACT_APP_GOOGLEKEY, "&libraries=places");
 		const scriptGoogle = document.createElement('script');
@@ -11,37 +11,28 @@ export default class GoogleMap extends Component {
 		scriptGoogle.src = url
 		window.document.body.appendChild(scriptGoogle)
 
-		//googleScript.addEventListener('load', {
 		scriptGoogle.addEventListener("load", () => {
-			this.googleMap = this.createGoogleMap();
+			this.googleMap = this.createGoogleMap()
 		})
 	}
 
-	createGoogleMap() {
-		console.log("about to getting position")
+	currentPosition() {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
-				currentpos.push({key: "lat", value: position.coords.latitude});
-				currentpos.push({key: "lng", value: position.coords.longitude});
-				console.log("Getting position from browser")
-			});
-		} else {
-			console.log("it doesn't work");
-			currentpos.push({key: "lat", value: 4.6097100 });
-			currentpos.push({key: "lng", value: -74.0817500});
+			    currentpos["lat"] = parseFloat(position.coords.latitude)
+			    currentpos["lng"] = parseFloat(position.coords.longitude)
+			})
 		}
-		new window.google.maps.Map(this.googleMapRef.current, {
-			zoom: 16,
-			center: currentpos,
-			disableDefaultUI: false, 
-		})
-	    }
+
+	createGoogleMap = () =>
+	    new window.google.maps.Map(this.googleMapRef.current, initialmap);
 
 	createMarker = () =>
 		new window.google.maps.Marker({
-			position: currentpos,
+			position: {"lat" : parseFloat(currentpos.lat), "lng": parseFloat(currentpos.lng)},
 			map: this.googleMap,
 		})
+		
 
 	render (){
 		return ( 
