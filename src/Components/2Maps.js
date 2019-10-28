d4dbb3150f80a0c98d0596e7a5793b4fdf8b273f
Juan Diego Alejandro Valencia Pena <juadiegoalejandro@hotmail.com>
2019-10-28 18:27:10 -0500

    fix map

--- src/Components/2Maps.js
+++ src/Components/2Maps.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 const currentpos = [];
 let initialmap = { zoom: 16, center: currentpos, disableDefaultUI: False};
 export default class GoogleMap extends Component {
@@ -18,11 +18,32 @@ export default class GoogleMap extends Component {
 
 	currentPosition() {
 		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-			    currentpos["lat"] = parseFloat(position.coords.latitude)
-			    currentpos["lng"] = parseFloat(position.coords.longitude)
-			})
-		}
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
 
 	createGoogleMap = () =>
 	    new window.google.maps.Map(this.googleMapRef.current, initialmap);
@@ -31,16 +52,15 @@ export default class GoogleMap extends Component {
 		new window.google.maps.Marker({
 			position: {"lat" : parseFloat(currentpos.lat), "lng": parseFloat(currentpos.lng)},
 			map: this.googleMap,
-		})
-		
+		});
 
-	render (){
-		return ( 
-			<div
-				id="google-map"
-				ref={this.googleMapRef}
-				style={{ width: '100%', height: '100%' }}
-			/>
-		)
-	}
-}
+		render (){
+			return ( 
+				<div
+					id="google-map"
+					ref={this.googleMapRef}
+					style={{ width: '100%', height: '100%' }}
+				/>
+			)
+		}
+	}}*/
