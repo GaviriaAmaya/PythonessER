63a1431f501b94703ebcfe30ed8cbf64b6e80aee
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-10-30 06:53:41 +0000

    [Fix] Uploading Maps again

---
+++ src/Components/Maps.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as FacilityData from "./data/ERPlaces.json";
+
+function MapwithMarkers() {
+  const [selectedFacility, setSelectedFacility] = useState(null);
+
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {FacilityData.features.map(facility => (
+        <Marker
+          key={facility.properties.PARK_ID}
+          position={{
+            lat: facility.coordinates.lat,
+            lng: facility.coordinates.lng
+          }}
+          onClick={() => {
+            setSelectedFacility(facility);
+          }}
+        />
+      ))}
+
+      {selectedFacility && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedFacility(null);
+          }}
+          position={{
+            lat: selectedFacility.coordinates.lat,
+            lng: selectedPark.coordinates.lng
+          }}
+        >
+          <div>
+            <h2>{selectedFacility.properties.name}</h2>
+            <p>{selectedFacility.properties.address}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(MapwithMarkers));
+
+export default function Map() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_GOOGLEKEY
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
+
