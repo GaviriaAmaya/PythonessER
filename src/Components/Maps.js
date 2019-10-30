faa62c3bb2d8ef47c4be3e8d1075a4c98c758c96
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-10-30 06:33:20 +0000

    [Fix] Fixing again

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -1,62 +1,66 @@
-import React, { useState, useEffect } from "react";
-import { withGoogleMap,withScriptjs, GoogleMap, Marker, InfoWindow } from "react-google-maps";
-import * as Facilitydata from "./data/ERPlaces.json";
+import React, { Component } from 'react';
+let currentpos = [];
+export default class GoogleMap extends Component {
+	
+	state = {
+		'currentpos': {'lat': 0, 'lng': 0}
+	}
 
-function MapwithMarkers() {
-  const [selectedFacility, setSelectedFacility] = useState(null);
+	googleMapRef = React.createRef()
+	
+	
+	componentDidMount () {
+		const url = "https://maps.googleapis.com/maps/api/js?key=".concat(process.env.REACT_APP_GOOGLEKEY, "&libraries=places");
+		const scriptGoogle = document.createElement('script');
+		scriptGoogle.type = 'text/javascript'
+		scriptGoogle.src = url
+		window.document.body.appendChild(scriptGoogle)
 
-  return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
-    >
-      {Facilitydata.info.map(facility => (
-        <Marker
-          key={facility.properties.Facility_ID}
-          position={{
-            lat: facility.geometry.coordinates.lat,
-            lng: facility.geometry.coordinates.lng
-          }}
-          onClick={() => {
-            setSelectedFacility(Facility);
-          }}
-        />
-      ))}
+		//googleScript.addEventListener('load', {
+		scriptGoogle.addEventListener("load", () => {
+			this.googleMap = this.createGoogleMap();
+		})
+	}
 
-      {selectedFacility && (
-        <InfoWindow
-          onCloseClick={() => {
-            setSelectedFacility(null);
-          }}
-          position={{
-            lat: selectedFacility.geometry.coordinates.lat,
-            lng: selectedFacility.geometry.coordinates.lng
-          }}
-        >
-          <div>
-            <h2>{selectedFacility.properties.NAME}</h2>
-            <p>{selectedFacility.properties.address}</p>
-          </div>
-        </InfoWindow>
-      )}
-    </GoogleMap>
-  );
+	createGoogleMap() {
+		console.log("about to getting position")
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				let currentpos = {'lat': position.coords.latitude, 'lng':position.coords.longitude}
+				currentpos = {'lat': 4.6097100, 'lng':-74.0817500}				
+				new window.google.maps.Map(this.googleMapRef.current, {
+					zoom: 16,
+					center: currentpos,
+					disableDefaultUI: false, 
+				})
+				this.setState({'currentpos': currentpos})
+				console.log("Getting position from browser")
+			});
+		} else {
+			console.log("it doesn't work");
+			let currentpos = {'lat': 4.6097100, 'lng':-74.0817500}
+			this.setState({'currentpos': currentpos})
+		}
+		new window.google.maps.Map(this.googleMapRef.current, {
+			zoom: 16,
+			center: this.state.currentpos,
+			disableDefaultUI: false, 
+		})
+	    }
+
+	createMarker = () =>
+		new window.google.maps.Marker({
+			position: currentpos,
+			map: this.googleMap,
+		})
+
+	render (){
+		return ( 
+			<div
+				id="google-map"
+				ref={this.googleMapRef}
+				style={{ width: '100%', height: '100%' }}
+			/>
+		)
+	}
 }
-
-const MapWrapped = withScriptjs(withGoogleMap(MapwithMarkers));
-
-export default function Map() {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_GOOGLE_KEY
-        }`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </div>
-  );
-}
-
