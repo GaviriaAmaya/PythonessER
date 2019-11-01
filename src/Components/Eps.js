c64d1d02bd50b1dc0ef6459b62b3f7440b416621
Juan Diego Alejandro Valencia Pena <juadiegoalejandro@hotmail.com>
2019-11-01 04:15:08 -0500

    new map

--- src/Components/Eps.js
+++ src/Components/Eps.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 export default class Epslist extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            eps:null
+        };
+    }
+
     render() {
         return(
-        <form onSubmit={this.onSubmit}>
+        <form onSubmit={this.setState}>
             <input type="text" 
                 placeholder="EPS" 
                 name="title"
